6982521dda7cffdb3b1c000eb7466a8ac09be169
LuisGustavoFA <luis.gustavo.freitas.a@gmail.com>
2025-06-12 11:17:02 -0300

    create dialog for borrow and return, add link to author through book card

--- src/components/BookCard.vue
+++ src/components/BookCard.vue
@@ -21,7 +21,10 @@
 
       <q-card-section class="q-pt-none">
         <div class="text-subtitle1">
-          Escrito por {{ this.authorName }}
+          Escrito por
+            <span class="subtitle-author" @click="this.$router.push(`/autor/${this.book.AUTHOR_ID}`)">
+              {{ this.authorName }}
+            </span>
         </div>
         <div class="text-caption text-grey">
           Editora {{ this.editorName }}
@@ -35,7 +38,7 @@
           <q-btn flat style="color: green;" v-if="emprestado == true" @click="onReturn">
             Devolver
           </q-btn>
-          <q-btn flat color="primary" @click="onBorrow()" v-else>
+          <q-btn flat color="primary" @click="onBorrow" v-else>
             Fazer empréstimo
           </q-btn>
         </div>
@@ -49,7 +52,7 @@
 
 <script>
 import { userStore } from 'src/stores/userStore';
-import { Notify } from 'quasar';
+import { Notify, useQuasar } from 'quasar';
 import axios from 'axios';
 import { getEmprestimoID, addEmprestimo } from 'src/services/borrowServices.js'
 
@@ -79,38 +82,86 @@ import { getEmprestimoID, addEmprestimo } from 'src/services/borrowServices.js'
       this.getEmprestimoState()
     },
 
+    setup() {
+      const $q = useQuasar();
+      function confirmBorrow() {
+        return new Promise((res, rej) => {
+          $q.dialog({
+            title: 'Empréstimo',
+            message: 'Deseja realizar um empréstimo deste livro?',
+            cancel: true,
+            persistent: true
+          }).onOk(() => {
+            res();
+          }).onCancel(() => {
+            rej();
+          })
+        })
+      }
+
+      function confirmReturn() {
+        return new Promise((res, rej) => {
+          $q.dialog({
+            title: 'Devolver',
+            message: 'Deseja retornar este livro?',
+            cancel: true,
+            persistent: true
+          }).onOk(() => {
+            res();
+          }).onCancel(() => {
+            rej();
+          })
+        })
+      }
+
+      return { confirmBorrow, confirmReturn }
+    },
+
     methods: {
     async onBorrow() {
-      if (userStore.user) {
-        let emprestimo_id = await addEmprestimo(this.book.ID)
-        if (emprestimo_id) {
-          Notify.create('Empréstimo realizado com sucesso!')
-          this.emprestado = true;
-          this.emprestimoID = emprestimo_id
+      this.confirmBorrow()
+      .then(async ()=> {
+        if (userStore.user) {
+          let emprestimo_id = await addEmprestimo(this.book.ID)
+          if (emprestimo_id) {
+            Notify.create('Empréstimo realizado com sucesso!')
+            this.emprestado = true;
+            this.emprestimoID = emprestimo_id
+          } else {
+            this.emprestado = false;
+          }
         } else {
-          this.emprestado = false;
+          Notify.create("É necessário estar logado para realizar essa ação!");
+          this.$router.push("/login");
         }
-      } else {
-        Notify.create("É necessário estar logado para realizar essa ação!");
-        this.$router.push("/login");
-      }
+      })
+      .catch(()=> {
+        console.log("Empréstimo cancelado!");
+      })
     },
 
     onReturn() {
-      axios.delete(`http://localhost:3000/emprestimos/${this.emprestimoID}`, {
-        headers: {
-          Authorization: `Bearer ${userStore.jwt}`
-        }
+      this.confirmReturn()
+      .then(()=> {
+        axios.delete(`http://localhost:3000/emprestimos/${this.emprestimoID}`, {
+          headers: {
+            Authorization: `Bearer ${userStore.jwt}`
+          }
+        })
+        .then(() => {
+          Notify.create("Livro devolvido com sucesso!");
+          this.emprestado = false;
+          this.emprestimoID = null;
+        })
+        .catch((error) => {
+          console.log(error);
+          Notify.create("Erro ao tentar devolver o livro.");
+        });
       })
-      .then(() => {
-        Notify.create("Livro devolvido com sucesso!");
-        this.emprestado = false;
-        this.emprestimoID = null;
+      .catch(()=> {
+        console.log("Retorno cancelado!");
       })
-      .catch((error) => {
-        console.log(error);
-        Notify.create("Erro ao tentar devolver o livro.");
-      });
+
     },
 
     getAuthor(id) {
@@ -164,5 +215,12 @@ import { getEmprestimoID, addEmprestimo } from 'src/services/borrowServices.js'
 </script>
 
 <style>
+.subtitle-author {
+  color: var(--q-primary);
+  cursor: pointer;
+}
 
+.subtitle-author:hover {
+  text-decoration: underline;
+}
 </style>
